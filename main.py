fa3657200d8bb7db9563d14067c1ba38e10db844
snehkr <snehkr.official@gmail.com>
2025-04-27 14:48:45 +0530

    HushTag V1.1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
 from app import create_app
-from gevent.pywsgi import WSGIServer
+
+# from gevent.pywsgi import WSGIServer
 
 
 app = create_app()
@@ -9,10 +10,10 @@ app.secret_key = "5neh_5hrut1_r1nk1"
 
 
 if __name__ == "__main__":
-    # host = app.config.get("HOST", "0.0.0.0")
-    # port = app.config.get("PORT", 5000)
-    # debug = app.config.get("DEBUG", True)
-    # app.run(host=host, port=port, debug=debug)
+    host = app.config.get("HOST", "0.0.0.0")
+    port = app.config.get("PORT", 5000)
+    debug = app.config.get("DEBUG", True)
+    app.run(host=host, port=port, debug=debug)
 
-    http_server = WSGIServer(("", 5000), app)
-    http_server.serve_forever()
+    # http_server = WSGIServer(("", 5000), app)
+    # http_server.serve_forever()
